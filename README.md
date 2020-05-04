ecb06f9ba033a29c1a72d7c87f3569663163b530
Dolan Hêriş <dolanskurd@mail.com>
2020-05-05 01:09:41 +0300

    updating whole files

--- README.md
+++ README.md
@@ -1,11 +1,63 @@
-# Dictio
+# Dictio English / Kurdish Open Dictionary for Golden Dictionary (Online Version)
+
 ## Using Online Dictio Dictionary on Golden Dictionary
 
+## Install GoldenDict
+
+- ### Linux
+
+  [**Download GoldenDict**](https://github.com/goldendict/goldendict)
+
+- ### Windows
+
+  [**Download GoldenDict (Free)**](https://sourceforge.net/projects/goldendict/files/early%20access%20builds/) 
+
+- ### MacOS
+
+  [**Download GoldenDict (Free)**](https://sourceforge.net/projects/goldendict/files/early%20access%20builds/)
+
+
+## Instructions
+
+1. Download the files from `https://github.com/dolanskurd/Dictio.git`
+2. Unzip the package.
+3. Open `GlodenDict.exe`
+4. On the top menu select `Edit/Dictionaries`
+5. Find `Programs` tab and click on it, then click on `add` botton
+6. Under the `Type` tab, select `html` from the drop-down menu and tick the `Enabled` option
+7. Under the `Name` tab, write a name for the Dictionary e.g.: `Dictio`
+8. Under the `Command Line` tab, first write Python (the version need to be specified) as the executer program and then the location of your python file, here it is `dictio.py` file, e.g. on Linux: `python3 "/home/$USER/Dictio/dictio.py"`. Don't remove the quotations! for Windows user, it doesn't need to write the python version e.g.:`python "C:\Users\USER\Desktop\Dictio\dictio.py"`, but `Python3` must be used.
+9. Under the `icon` tab, you can specify an icon for your dictionary via providing complete location of the file. This part is OPTIONAL.
+10. Click on `Apply`, then `OK`.
+11. Done.
+
+## Screenshots
+
+![]()
+
+![]()
+
+![]()
+
+
 ### Dictio Dictionary on its website [http://dictio.kurditgroup.org/dictio]:
-![Dictio Dictionary on its web page](https://github.com/dolanskurd/Dictio/blob/master/docs/img/02.jpg)
 
-### Dictio inside the Golden Dictionary (Online Version)
-![Dictio inside the Golden Dictionary](https://github.com/dolanskurd/Dictio/blob/master/docs/img/01.jpg)
+![]()
+
+### Dictio Dictionary on GoldenDict:
+
+![]()
 
 
-### User Guide will be added soon !
+### Fonts needed:
+
+- [Hack](https://github.com/source-foundry/Hack)
+- [Estedad](https://aminabedi68.github.io/Estedad/)
+- [Vazir](https://rastikerdar.github.io/vazir-font/)
+
+## Contact me
+
+I hope you like this dictionary. Feel free to reach out if you have questions or if you want to contribute in any way:
+
+- **[Twitter](http://www.twitter.com/dolanskurd)**
+- **E-mail: [dolanskurd@mail.com](mailto:dolanskurd@mail.com)**
--- dictio.py
+++ dictio.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 """
 
-Dictio for Golden Dictionary (Online Version)
+Dictio English / Kurdish Open Dictionary for Golden Dictionary (Online Version)
 
-author= Dolan Hêriş
+author = Dolan Hêriş
 
-author_email= dolanskurd@mail.com
+author_email = dolanskurd@mail.com
 
 GitHub: https://github.com/dolanskurd/Dictio
 
@@ -15,87 +18,152 @@ import sys
 import io
 from bs4 import BeautifulSoup
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 
-root_url = 'http://dictio.kurditgroup.org/dictio/'
-word = input('\r\n')
+root_url = "https://dictio.kurditgroup.org/dictio/"
+word = input("\r\n")
 url = requests.get(root_url + word).text
-soup = BeautifulSoup(url, 'lxml')
+soup = BeautifulSoup(url, "lxml")
 
 
 css = """<style type="text/css">
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a .related-translation-title {
-        font-size: 12pt;
-        color: navy;
-        direction: rtl;
-}
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a .kig_tran {
-        direction: rtl;
-}
-
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a .kig_val, .en {
-        direction: ltr;
-        font-size: 12pt;
-        color: navy;
-}
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a #top-title, p.bold {
-        direction: rtl;
+.title_en {
+        font-family: hack, Verdana, sans-serif;
+        font-size: 13pt;
+        font-weight: bold;
+        color: #101010;
         text-align: center;
-        font-size: 12pt;
-        color: navy;
-        font-family: 'Times New Roman';
-}
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a .related-translation-title {
+        line-height: 1.8;
+    }
+    .title_ku {
+        font-family: estedad, vazir, arial, tahoma, sans-serif;
+        font-size: 13pt;
         font-weight: bold;
-}
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a .ku {
-        font-size: 12pt;
-        
-}
-
-#gdarticlefrom-8760b966b9972903e0a7d6fc4519da4a .word {
-        font-size: 12pt;
+        color: #990000;
+        text-align: center;
+        line-height: 1.8;
+    }
+    .en_entry {
+        font-family: hack, Verdana, sans-serif;
+        font-size: 14pt;
+        color: #314089;
         font-weight: bold;
-}
+    }
+    .ku_entry {
+        font-family: estedad, vazir, arial, tahoma, sans-serif;
+        font-size: 14pt;
+        color: #314089;
+        font-weight: bold;
+        direction: rtl;
+    }
+    .ku {
+        font-family: estedad, vazir, arial, tahoma, sans-serif;
+        font-size: 12pt;
+        color: #3B3E41;
+        font-weight: bold;
+        direction: rtl;
+    }
+    .en_meaning {
+        font-family: estedad, vazir, arial, tahoma, sans-serif;
+        font-size: 12pt;
+        color: #3B3E41;
+        font-weight: bold;
+    }
+    .en {
+        font-family: Hack, Verdana, sans-serif;
+        color: #333333;
+        padding: 7px 7px;
+        text-align: center;
+        display: inline-block;
+        font-size: 12px;
+        font-weight: bold;
+        margin: 4px 2px;
+        background-color: #EDEDED;
+        border: 2px solid #EDEDED;
+        border-radius: 3px;
+    }
+    
+    .ku_equ {
+        font-family: estedad, vazir, arial, tahoma, sans-serif;
+        color: #333333;
+        padding: 7px 7px;
+        text-align: center;
+        font-size: 12px;
+        font-weight: bold;
+        margin: 4px 2px;
+        background-color: #EDEDED;
+        border: 2px solid #EDEDED;
+        border-radius: 3px;
+        direction: rtl;
+        float: right;
+    }
+    
+    .syn{
+        font-family: hack, Verdana, sans-serif;
+        font-size: 12pt;
+        color: #ff2500;
+        font-weight: bold;
+        line-height: 2;
+    }
+    
+    .ku_syn{
+        font-family: estedad, vazir, arial, tahoma, sans-serif;
+        font-size: 12pt;
+        color: #ff2500;
+        font-weight: bold;
+        line-height: 2.5;
+        margin: 0 75px 0 0;
+        direction: rtl;
+    }
+    hr{ 
+        border: 0;
+        height: 1px;
+        background: #333;
+        background-image: linear-gradient(to right, #ccc, #333, #ccc);
+    }
 </style>"""
 
-print(css, '\n')
+print(css, "<br>")
+print(css, '<div class="title_en"> Dictio English / Kurdish Open Dictionary </div>')
+print(css, '<div class="title_ku"> فەرهەنگی کراوەی کوردی / ئینگلیزی دیکتیۆ </div>')
+print(css, "<br>")
 
-dictio_title = soup.find('div', id='top-title')
-print(css, dictio_title)
+if word.isascii():
+    print(css, "<br>")
+    print(css, '<div class="en_entry">' + word + "</div>")
+    print(css, "<br>")
+    for container in soup.find_all("div", "col-md-8 offset-md-2"):
+        for ul in container.find_all("ul"):
+            for li in ul.find_all("li", class_=["list-group-item", "rtl"]):
+                for ku_meaning in li.find_all("span", "dictio-result-item"):
+                    print(css, '<div class="ku">' + ku_meaning.text + "</div>")
+                    print(css, "<br>")
+                print(css, '<div class="syn">' + "Synonyms: " + "</div>")
+                for en_equ in li.find_all("span", "text-secondary bg-light pl-2 pr-2"):
+                    print(css, '<span class="en">' + en_equ.text + "</span>")
+                print(css, "<br>")
+                print(css, "<hr>")
+                print(css, "<br>")
+else:
+    print(css, "<br>")
+    print(css, '<div class="ku_entry">' + word + "</div>")
+    print(css, "<br>")
+    for en_meaning in soup.find_all("div", "col-md-8 offset-md-2"):
+        for ul in en_meaning.find_all("ul"):
+            for li in ul.find_all("li", class_=["list-group-item", "ltr"]):
+                for en_meaning in li.find_all("span", "dictio-result-item"):
+                    print(css, '<div class="en_meaning">' + en_meaning.text + "</div>")
+                    print(css, "<br>")
+                print(
+                    css, '<div class="ku_syn">' + "هاوواتاکان: " + "</div>",
+                )
+                for ku_equ in li.find_all("span", "text-secondary bg-light pl-2 pr-2"):
+                    print(
+                        css, '<div class="ku_equ">' + ku_equ.text + "</div>",
+                    )
 
-dictio_title2 = soup.find('p', 'bold')
-print(css, dictio_title2)
-print(css, '\n')
+                print(css, "<br>" * 4)
+                print(css, "<hr>")
+                print(css, "<br>")
 
-print('<div class="word">' + str(word) + '</div>')
-print(css, '\n')
-
-
-meaning = soup.find('h2')
-if meaning == None:
-        print(css, '\n')
-else:  
-        print(css, meaning)
-print(css, '\n')
-print(css, '<hr>')
-
-extra = soup.find('div', 'related-translation-title')
-print(css, extra)
-print(css, '\n')
-
-for all_related in soup.findAll('div', 'translation clear index parent'):
-    en_word = all_related.find('div', attrs={'class': 'kig_val index en'})
-    ku_word = all_related.find('div', attrs={'class': 'kig_tran ku'})
-    if en_word == None or ku_word == None:
-            print(css, '\n')
-    else:     
-            print(css, en_word, ku_word)
-            print(css, '<hr>')
-print(css, '\n')
+    print(css, "<br>")
